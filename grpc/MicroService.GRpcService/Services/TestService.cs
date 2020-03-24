acfbb536122ec411137757db35f3c17bf7e3a02d
罗朝敏 <shanliujiu@foxmail.com>
2020-03-24 11:55:04 +0800

    grpc

---
+++ grpc/MicroService.GRpcService/Services/TestService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcTest;
+
+namespace MicroService.GRpcService.Services
+{
+    public class TestService : Test.TestBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
