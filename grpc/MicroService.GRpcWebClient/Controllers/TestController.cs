343234b6a8cdfe8fd7dd5a1ab6cf17b32606fd03
罗朝敏 <shanliujiu@foxmail.com>
2020-03-24 21:10:32 +0800

    grpc 进阶模式

---
+++ grpc/MicroService.GRpcWebClient/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GrpcTest;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService.GRpcWebClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly Test.TestClient _testClient;
+
+        public TestController(Test.TestClient testClient)
+        {
+            _testClient = testClient;
+        }
+
+        [Route("get")]
+        public async Task<string> Get(string name)
+        {
+            var res = await _testClient.SayHelloAsync(new HelloRequest() { Name = name });
+            return res.Message;
+        }
+    }
+}
